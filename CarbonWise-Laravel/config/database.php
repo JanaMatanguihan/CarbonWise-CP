70fcbedcee8a73a9234396d006915cd80e8bddf4
JanaMatanguihan <janamatanguihan.edu@gmail.com>
2026-09-11 00:44:07 +0800

    Add Neon database connection

--- CarbonWise-Laravel/config/database.php
+++ CarbonWise-Laravel/config/database.php
@@ -106,6 +106,21 @@ return [
             // 'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'false'),
         ],
 
+        'neon' => [
+            'driver' => 'pgsql',
+            'url' => env('DATABASE_URL'),
+            'host' => env('DB_HOST'),
+            'port' => env('DB_PORT', '5432'),
+            'database' => env('DB_DATABASE'),
+            'username' => env('DB_USERNAME'),
+            'password' => env('DB_PASSWORD'),
+            'charset' => 'utf8',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'search_path' => 'public',
+            'sslmode' => 'require',
+        ],
+
     ],
 
     /*
