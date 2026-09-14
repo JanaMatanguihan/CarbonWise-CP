7423474b9eff85b2f35d83ba820604702ff6d230
Janelle Rabino <janelle.rabino44@gmail.com>
2026-09-14 22:47:56 +0800

    Fix Neon PostgreSQL connection

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use App\Database\Connectors\NeonPostgresConnector;
+use Illuminate\Database\PostgresConnection;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +22,17 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        DB::extend('pgsql', function ($config, $name) {
+            $connector = new NeonPostgresConnector();
+
+            $pdo = $connector->connect($config);
+
+            return new PostgresConnection(
+                $pdo,
+                $config['database'],
+                $config['prefix'] ?? '',
+                $config
+            );
+        });
     }
 }
